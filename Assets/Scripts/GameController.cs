2f71f299b28335d53fae2f5a0d95c0b508ba0c76
fan you <youfan.noey@gmail.com>
2015-01-24 15:31:45 -0500

    ls

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,20 +8,31 @@ public class GameController : MonoBehaviour {
 	private float movey = 0f;
 
 	public float Speed = 0f;
+	public bool walk;
 
 	// Use this for initialization
 	void Start () {
 		Screen.showCursor = false;
+		walk = false;
 		cursor = GameObject.Find ("Cursor");
 		player = gameObject;
 	}
 
 	void FixedUpdate () {
-		movex = Input.GetAxis ("Horizontal");
-		movey = Input.GetAxis ("Vertical");
-		player.rigidbody2D.velocity = new Vector2 (movex * Speed, movey * Speed);
+		if(walk) {
+			movex = Input.GetAxis ("Horizontal");
+			movey = Input.GetAxis ("Vertical");
+			player.rigidbody2D.velocity = new Vector2 (movex * Speed, movey * Speed);
+		}
 	}
-	
+
+	void EnableWalk() {
+		walk = true;
+	}
+
+	void DisableWalk() {
+		walk = false;
+	}
 	// Update is called once per frame
 	void Update () {
 		cursor.transform.position = (Input.mousePosition - new Vector3(Screen.width/2f, Screen.height/2f, 0)) / 100;
