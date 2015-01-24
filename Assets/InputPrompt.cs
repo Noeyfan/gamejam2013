089d2cf44099cc7b849dd720b77b1088dbcf3120
Tim Shen <timshen91@gmail.com>
2015-01-23 21:41:55 -0800

    InputPrompt and OutputPrompt.

---
+++ Assets/InputPrompt.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputPrompt : MonoBehaviour {
+	public int screenPositionX;
+	public int screenPositionY;
+	private string buffer = "";
+	private OutputPrompt output;
+
+	void Start() {
+		output = GameObject.Find ("OutputPrompt").GetComponent<OutputPrompt> ();
+	}
+
+	void OnGUI() {
+		Event e = Event.current;
+		if (e.isKey && e.keyCode == KeyCode.Return) {
+			eval(buffer);
+			buffer = "";
+		}
+		buffer = GUI.TextField (new Rect (screenPositionX, screenPositionY, Screen.width, 20), buffer);
+	}
+	
+	void eval(string cmd) {
+		if (cmd == "") {
+			return;
+		}
+		output.content = "Simple echo: " + cmd;
+	}
+}
---
+++ Assets/OutputPrompt.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OutputPrompt : MonoBehaviour {
+	public int screenPositionX;
+	public int screenPositionY;
+	public string content;
+	
+	void OnGUI() {
+		GUI.Label(new Rect(screenPositionX, screenPositionY, Screen.width, 20), content);
+	}
+}
