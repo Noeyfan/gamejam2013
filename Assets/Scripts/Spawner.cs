ced46fa53154266a37fdf0dac12bf493baffb8c0
Tim Shen <timshen91@gmail.com>
2015-01-24 21:30:12 -0800

    Spawner

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+	public GameObject item;
+	public Vector2 velocity;
+	public float coolDownSeconds;
+	private float lastTime = 0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (lastTime + coolDownSeconds < Time.time) {
+			lastTime = Time.time;
+			var newItem = Instantiate(item) as GameObject;
+			newItem.rigidbody2D.velocity = velocity;
+		}
+	}
+}
