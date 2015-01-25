9d7875327ec9e3f6d461fe655bd3fa1479c96107
Tim Shen <timshen91@gmail.com>
2015-01-25 11:10:13 -0800

    key color

--- Assets/Scripts/keyController.cs
+++ Assets/Scripts/keyController.cs
@@ -2,8 +2,10 @@
 using System.Collections;
 
 public class keyController : MonoBehaviour {
+	private OutputPrompt output;
 	// Use this for initialization
 	void Start () {
+		output = GameObject.Find ("OutputPrompt").GetComponent<OutputPrompt> ();
 	}
 	
 	// Update is called once per frame
@@ -16,6 +18,17 @@ public class keyController : MonoBehaviour {
 		if(c.tag == "Player") {
 			c.GetComponent<GameController> ().GetKey (gameObject.name);
 			c.GetComponent<GameController> ().PlayGetKey();
+			string color = "";
+			if (name == "key3(Clone)") {
+				color = "blue";
+			} else if (name == "key1(Clone)") {
+				color = "green";
+			} else if (name == "key2(Clone)") {
+				color = "red";
+			} else if (name == "key4(Clone)") {
+				color = "yellow";
+			}
+			output.SendEvent("you got " + color + " key.");
 			//gameObject.GetComponent<AudioSource>().Play();
 			Destroy (gameObject);
 		}
