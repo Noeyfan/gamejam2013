9ff800bddf4e2995b0338014a2b5109cff7c1dd9
Tim Shen <timshen91@gmail.com>
2015-01-24 16:32:50 -0800

    Tutorial.

--- Assets/Scripts/DialogueBehavior.cs
+++ Assets/Scripts/DialogueBehavior.cs
@@ -10,6 +10,8 @@ public class DialogueBehavior : MonoBehaviour {
 	private static Dictionary<string, DialogueTree> trees = new Dictionary<string, DialogueTree>();
 	private DialogueTree.Node state;
 
+	private bool activated = false;
+
 	static DialogueBehavior() {
 		trees.Add("adam", InitAdamDialogue ());
 	}
@@ -36,7 +38,7 @@ public class DialogueBehavior : MonoBehaviour {
 		dialogue.AddOption ("where", "Very well. Oh, I can surely smell *regret*, though.", "what");
 		dialogue.AddOption ("where", "Damn it. I curse you.", "curse");
 
-		dialogue.SetNodeContent ("curse", "Curse? Hahahahhaha...curse, of course. Obviously I *am* the mostly cursed one. I *am* the one who lost the motality. They call me The Nameless One.");
+		dialogue.SetNodeContent ("curse", "Curse? Hahahahhaha...curse, of course. Obviously I *am* the mostly cursed one.\nI *am* the one who lost the motality. They call me The Nameless One.");
 		dialogue.AddOption ("curse", "Oh, you are The Nameless One! The one who never dies!", "what");
 
 		dialogue.SetNodeContent ("what", "Whatever. What do we do now?");
@@ -47,6 +49,9 @@ public class DialogueBehavior : MonoBehaviour {
 	}
 
 	void OnGUI() {
+		if (!activated) {
+			return;
+		}
 		GUI.Label(dialogueRect, state.ToString(), gui_style);
 	}
 
@@ -69,10 +74,22 @@ public class DialogueBehavior : MonoBehaviour {
 	}
 
 	public void Talk(string content) {
+		if (!activated) {
+			return;
+		}
 		try {
 			int choice = System.Convert.ToInt32(content);
 			state = state.Next (choice-1);
 		} catch {
 		}
+	}
+
+	public void Activate() {
+		activated = true;
+		Reset ();
+	}
+
+	public void Deactivate() {
+		activated = false;
 	}
 }
